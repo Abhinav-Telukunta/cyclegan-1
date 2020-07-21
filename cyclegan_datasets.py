e0c1db627f596e62da471c318dafbb85694de2c4
Telukunta Vijay Abhinav <62216101+Abhinav-Telukunta@users.noreply.github.com>
2020-07-21 15:26:51 +0530

    Update cyclegan_datasets.py

--- cyclegan_datasets.py
+++ cyclegan_datasets.py
@@ -3,18 +3,18 @@
 """The size of each dataset. Usually it is the maximum number of images from
 each domain."""
 DATASET_TO_SIZES = {
-    'horse2zebra_train': 1334,
-    'horse2zebra_test': 140
+    'monet2photo_train': 7359,
+    'monet2photo_test': 872
 }
 
 """The image types of each dataset. Currently only supports .jpg or .png"""
 DATASET_TO_IMAGETYPE = {
-    'horse2zebra_train': '.jpg',
-    'horse2zebra_test': '.jpg',
+    'monet2photo_train': '.jpg',
+    'monet2photo_test': '.jpg',
 }
 
 """The path to the output csv file."""
 PATH_TO_CSV = {
-    'horse2zebra_train': './CycleGAN_TensorFlow/input/horse2zebra/horse2zebra_train.csv',
-    'horse2zebra_test': './CycleGAN_TensorFlow/input/horse2zebra/horse2zebra_test.csv',
+    'horse2zebra_train': './CycleGAN_TensorFlow/input/horse2zebra/monet2photo_train.csv',
+    'horse2zebra_test': './CycleGAN_TensorFlow/input/horse2zebra/monet2photo_test.csv',
 }
